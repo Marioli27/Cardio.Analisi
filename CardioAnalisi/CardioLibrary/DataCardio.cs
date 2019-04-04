80dbe2bafd6c2af56eea1f9d194f09621c673a58
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-04 09:14:30 +0100

    Abbiamo fatto i primi due punti dell'esercizio

--- CardioAnalisi/CardioLibrary/DataCardio.cs
+++ CardioAnalisi/CardioLibrary/DataCardio.cs
@@ -8,5 +8,54 @@ namespace CardioLibrary
 {
     public class DataCardio
     {
+        public static string Battiti(int età)
+        {
+
+            if (età <= 0)
+            {
+                return "Impossibile";
+            }
+            else
+            {
+                double Battito_Massimo = 220 - età;
+                double Massimo_Consigliato = Battito_Massimo / 100 * 90;
+                double Minimo_Consigliato = Battito_Massimo / 100 * 70;
+                string Consigliato = ($"{Minimo_Consigliato};{Massimo_Consigliato}");
+                return Consigliato;
+
+            }  
+        }
+
+        public static string FrequenzaCardio (int battiti)
+        {
+            if(battiti<0)
+            {
+                risp = "Imposssibile";
+            }
+
+            if(battiti==0)
+            {
+                risp = "Morto";
+            }
+
+            if (battiti < 60 )
+            {
+                risp = "Bradicardia";
+            }
+
+            if (battiti >=60 && battiti<=100)
+            {
+                risp = "Normale";
+            }
+
+            if (battiti > 100)
+            {
+                risp= "Tachicardia";
+            }
+
+            return risp;
+        }
+
+
     }
 }
--- CardioAnalisi/DataCardio.Test/DataCardio1.cs
+++ CardioAnalisi/DataCardio.Test/DataCardio1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CardioLibrary;
 
 namespace DataCardio.Test
 {
@@ -7,8 +8,76 @@ namespace DataCardio.Test
     public class DataCardio1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestBattiti()
         {
+            int età = 18;
+            string asp = "141,4;181,8";
+            string consigliato = CardioLibrary.DataCardio.Battiti(età);
+            Assert.AreEqual(asp, consigliato);
+        }
+
+        [TestMethod]
+        public void TestBattiti1()
+        {
+            int età = 0;
+            string asp = "Impossibile";
+            string consigliato = CardioLibrary.DataCardio.Battiti(età);
+            Assert.AreEqual(asp, consigliato);
+
+        }
+
+        [TestMethod]
+        public void TestBattiti2()
+        {
+            int età = -5;
+            string asp = "Impossibile";
+            string consigliato = CardioLibrary.DataCardio.Battiti(età);
+            Assert.AreEqual(asp, consigliato);
+        }
+
+        [TestMethod]
+        public void TestFrequenza()
+        {
+            int battiti = 0;
+            string asp = "Morto";
+            string freguenza_Cardiaca = CardioLibrary.DataCardio.FrequenzaCardio(battiti);
+            Assert.AreEqual(asp,freguenza_Cardiaca);
+        }
+
+        [TestMethod]
+        public void TestFrequenza1()
+        {
+            int battiti = -5;
+            string asp = "Impossibile";
+            string freguenza_Cardiaca = CardioLibrary.DataCardio.FrequenzaCardio(battiti);
+            Assert.AreEqual(asp, freguenza_Cardiaca);
+        }
+
+        [TestMethod]
+        public void TestFrequenza2()
+        {
+            int battiti = 35;
+            string asp = "Bradicardia";
+            string freguenza_Cardiaca = CardioLibrary.DataCardio.FrequenzaCardio(battiti);
+            Assert.AreEqual(asp, freguenza_Cardiaca);
+        }
+
+        [TestMethod]
+        public void TestFrequenza3()
+        {
+            int battiti = 70;
+            string asp = "Normale";
+            string freguenza_Cardiaca = CardioLibrary.DataCardio.FrequenzaCardio(battiti);
+            Assert.AreEqual(asp, freguenza_Cardiaca);
+        }
+
+        [TestMethod]
+        public void TestFrequenza4()
+        {
+            int battiti = 120;
+            string asp = "Tachicardia";
+            string freguenza_Cardiaca = CardioLibrary.DataCardio.FrequenzaCardio(battiti);
+            Assert.AreEqual(asp, freguenza_Cardiaca);
         }
     }
 }

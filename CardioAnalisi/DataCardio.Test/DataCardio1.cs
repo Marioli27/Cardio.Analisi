ef37822544d8050987a00e7981844a8caba8547d
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-04 08:27:42 +0100

    Creazione blank solution

---
+++ CardioAnalisi/DataCardio.Test/DataCardio1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataCardio.Test
+{
+    [TestClass]
+    public class DataCardio1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
